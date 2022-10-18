f5dd9a9105dc3e09cb47f76482af68cf06c36c1c
AsmaaDev <a.muhammed3601@gmail>
2022-10-18 07:57:36 +0200

    admin layouts and includes

---
+++ admin/layouts/includes/sidebar.php
@@ -0,0 +1,120 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="index.html">
+              <i class="mdi mdi-grid-large menu-icon"></i>
+              <span class="menu-title">Dashboard</span>
+            </a>
+          </li>
+          <li class="nav-item nav-category">Admins</li>
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#admin" aria-expanded="false" aria-controls="admin">
+              <i class="menu-icon mdi mdi-floor-plan"></i>
+              <span class="menu-title">Admins</span>
+              <i class="menu-arrow"></i> 
+            </a>
+            <div class="collapse" id="admin">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="../admins/index.php">Admins</a></li>
+                <li class="nav-item"> <a class="nav-link" href="../admins/add_new.php">Add new</a></li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item nav-category">Users</li>
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#users" aria-expanded="false" aria-controls="users">
+              <i class="menu-icon mdi mdi-floor-plan"></i>
+              <span class="menu-title">Users</span>
+              <i class="menu-arrow"></i> 
+            </a>
+            <div class="collapse" id="users">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="../users/index.php">All users</a></li>
+                <li class="nav-item"> <a class="nav-link" href="../users/add_new.php">Add user</a></li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item nav-category">Brands</li>
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#brands" aria-expanded="false" aria-controls="brands">
+              <i class="menu-icon mdi mdi-floor-plan"></i>
+              <span class="menu-title">Brands</span>
+              <i class="menu-arrow"></i> 
+            </a>
+            <div class="collapse" id="brands">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="../brands/index.php">All Brands</a></li>
+                <li class="nav-item"> <a class="nav-link" href="../brands/add_new.php">Add new</a></li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item nav-category">Categories</li>
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#categories" aria-expanded="false" aria-controls="categories">
+              <i class="menu-icon mdi mdi-floor-plan"></i>
+              <span class="menu-title">categories</span>
+              <i class="menu-arrow"></i> 
+            </a>
+            <div class="collapse" id="categories">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="../categories/index.php">All Categories</a></li>
+                <li class="nav-item"> <a class="nav-link" href="../categories/add_new.php">Add new</a></li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item nav-category">Products</li>
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#products" aria-expanded="false" aria-controls="products">
+              <i class="menu-icon mdi mdi-floor-plan"></i>
+              <span class="menu-title">Products</span>
+              <i class="menu-arrow"></i> 
+            </a>
+            <div class="collapse" id="products">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="../products/index.php">All Products</a></li>
+                <li class="nav-item"> <a class="nav-link" href="../products/add_new.php">Add new</a></li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item nav-category">Orders</li>
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#orders" aria-expanded="false" aria-controls="orders">
+              <i class="menu-icon mdi mdi-floor-plan"></i>
+              <span class="menu-title">Orders</span>
+              <i class="menu-arrow"></i> 
+            </a>
+            <div class="collapse" id="orders">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="../orders/index.php">All Orders</a></li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item nav-category">payments</li>
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#payments" aria-expanded="false" aria-controls="payments">
+              <i class="menu-icon mdi mdi-floor-plan"></i>
+              <span class="menu-title">payments</span>
+              <i class="menu-arrow"></i> 
+            </a>
+            <div class="collapse" id="payments">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="../payments/index.php">All payments</a></li>
+              </ul>
+            </div>
+          </li>
+
+          <li class="nav-item nav-category">pages</li>
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+              <i class="menu-icon mdi mdi-account-circle-outline"></i>
+              <span class="menu-title">User Pages</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="auth">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="../auth/login.php"> Login </a></li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </nav>
